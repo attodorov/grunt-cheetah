046a0ef417415fb7234d37a7672353d9bca36864
Angel <attodorov@gmail.com>
2014-11-22 15:27:49 +0200

    new repo

---
+++ tasks/cheetah.js
@@ -0,0 +1,52 @@
+/*
+ * grunt-cheetah
+ * https://github.com/attodorov/grunt-cheetah
+ *
+ * Copyright (c) 2014 Angel Todorov
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('cheetah', 'Instrument code to performance test  it', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        } 
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+      // now instrument src with cheetah
+      //var cheetah = require("cheetah");
+      //console.log("CHEETAH: " + cheetah);
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
